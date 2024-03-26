1fbaa4be105f3b3aecae7d5d6f22c6699947ed0c
burak753 <burak753@RPC01>
2024-03-26 11:01:38 +0300

    Proje dosyası ekle.

---
+++ Uygulama22/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama22
+{
+    public partial class Form1 : Form
+    {
+        ArrayList kaynakVeri = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKitapEkle_Click(object sender, EventArgs e)
+        {
+            Kitaplar kitap = new Kitaplar();
+            kitap.KitapAdi = txtKitapAdi.Text;
+            kitap.KitapYazari = txtKitapYazari.Text;
+            kitap.KitapTuru = txtKitapTuru.Text;
+            kaynakVeri.Add(kitap);
+        }
+
+        class Kitaplar
+        {
+            public string KitapAdi { get; set; }
+            public string KitapYazari { get; set; }
+            public string KitapTuru { get; set; }
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            listeVeri.DataSource = null;
+            listeVeri.DataSource = kaynakVeri;
+            listeVeri.DisplayMember = "KitapAdi";
+        }
+    }
+}
